89e2f2f573d81c42134923822a71a38fda791f3a
Paola Quaino <pquaino@fiq.unl.edu.ar>
2026-08-06 20:43:46 -0300

    todo nuevo...

---
+++ gif-fixed.js
@@ -0,0 +1,452 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	var installedModules = {};
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 		module.loaded = true;
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	__webpack_require__.m = modules;
+/******/ 	__webpack_require__.c = installedModules;
+/******/ 	__webpack_require__.p = "";
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+	var _gifsparser = __webpack_require__(1);
+
+	if (typeof AFRAME === 'undefined') {
+	  throw 'Component attempted to register before AFRAME was available.';
+	}
+
+	var parseUrl = AFRAME.utils.srcLoader.parseUrl;
+	var debug = AFRAME.utils.debug;
+
+	debug.enable('shader:gif:warn');
+	var warn = debug('shader:gif:warn');
+	var log = debug('shader:gif:debug');
+
+	var gifData = {};
+
+	function createError(err, src) {
+	  return { status: 'error', src: src, message: err, timestamp: Date.now() };
+	}
+
+	AFRAME.registerShader('gif', {
+
+	  schema: {
+	    color: { type: 'color' },
+	    fog: { default: true },
+	    src: { default: null },
+	    autoplay: { default: true }
+	  },
+
+	  init: function init(data) {
+	    log('init', data);
+	    log(this.el.components);
+	    this.__cnv = document.createElement('canvas');
+	    this.__cnv.width = 2;
+	    this.__cnv.height = 2;
+	    this.__ctx = this.__cnv.getContext('2d');
+	    this.__texture = new THREE.Texture(this.__cnv);
+	    this.__material = {};
+	    this.__reset();
+	    this.material = new THREE.MeshBasicMaterial({ map: this.__texture });
+	    this.el.sceneEl.addBehavior(this);
+	    this.__addPublicFunctions();
+	    return this.material;
+	  },
+
+	  update: function update(oldData) {
+	    log('update', oldData);
+	    this.__updateMaterial(oldData);
+	    this.__updateTexture(oldData);
+	    return this.material;
+	  },
+
+	  tick: function tick(t) {
+	    if (!this.__frames || this.paused()) return;
+	    if (Date.now() - this.__startTime >= this.__nextFrameTime) {
+	      this.nextFrame();
+	    }
+	  },
+
+	  __updateMaterial: function __updateMaterial(data) {
+	    var material = this.material;
+	    var newData = this.__getMaterialData(data);
+	    Object.keys(newData).forEach(function (key) {
+	      material[key] = newData[key];
+	    });
+	  },
+
+	  __getMaterialData: function __getMaterialData(data) {
+	    return {
+	      fog: data.fog,
+	      color: new THREE.Color(data.color)
+	    };
+	  },
+
+	  __setTexure: function __setTexure(data) {
+	    log('__setTexure', data);
+	    if (data.status === 'error') {
+	      warn('Error: ' + data.message + '\nsrc: ' + data.src);
+	      this.__reset();
+	    } else if (data.status === 'success' && data.src !== this.__textureSrc) {
+	      this.__reset();
+	      this.__ready(data);
+	    }
+	  },
+
+	  __updateTexture: function __updateTexture(data) {
+	    var src = data.src;
+	    var autoplay = data.autoplay;
+
+	    if (typeof autoplay === 'boolean') {
+	      this.__autoplay = autoplay;
+	    } else if (typeof autoplay === 'undefined') {
+	      this.__autoplay = true;
+	    }
+	    if (this.__autoplay && this.__frames) {
+	      this.play();
+	    }
+
+	    if (src) {
+	      this.__validateSrc(src, this.__setTexure.bind(this));
+	    } else {
+	      this.__reset();
+	    }
+	  },
+
+	  __validateSrc: function __validateSrc(src, cb) {
+
+	    var url = parseUrl(src);
+	    if (url) {
+	      this.__getImageSrc(url, cb);
+	      return;
+	    }
+
+	    var message = void 0;
+
+	    var el = this.__validateAndGetQuerySelector(src);
+	    if (!el || (typeof el === 'undefined' ? 'undefined' : _typeof(el)) !== 'object') {
+	      return;
+	    }
+	    if (el.error) {
+	      message = el.error;
+	    } else {
+	      var tagName = el.tagName.toLowerCase();
+	      if (tagName === 'video') {
+	        src = el.src;
+	        message = 'For video, please use `aframe-video-shader`';
+	      } else if (tagName === 'img') {
+	        this.__getImageSrc(el.src, cb);
+	        return;
+	      } else {
+	        message = 'For <' + tagName + '> element, please use `aframe-html-shader`';
+	      }
+	    }
+
+	    if (message) {
+	      (function () {
+	        var srcData = gifData[src];
+	        var errData = createError(message, src);
+	        if (srcData && srcData.callbacks) {
+	          srcData.callbacks.forEach(function (cb) {
+	            return cb(errData);
+	          });
+	        } else {
+	          cb(errData);
+	        }
+	        gifData[src] = errData;
+	      })();
+	    }
+	  },
+
+	  __getImageSrc: function __getImageSrc(src, cb) {
+	    var _this = this;
+
+	    if (src === this.__textureSrc) {
+	      return;
+	    }
+
+	    var srcData = gifData[src];
+	    if (!srcData || !srcData.callbacks) {
+	      srcData = gifData[src] = { callbacks: [] };
+	      srcData.callbacks.push(cb);
+	    } else if (srcData.src) {
+	      cb(srcData);
+	      return;
+	    } else if (srcData.callbacks) {
+	      srcData.callbacks.push(cb);
+	      return;
+	    }
+	    var tester = new Image();
+	    tester.crossOrigin = 'Anonymous';
+	    tester.addEventListener('load', function (e) {
+	      _this.__getUnit8Array(src, function (arr) {
+	        if (!arr) {
+	          onError('This is not gif. Please use `shader:flat` instead');
+	          return;
+	        }
+	        (0, _gifsparser.parseGIF)(arr, function (times, cnt, frames) {
+	          var newData = { status: 'success', src: src, times: times, cnt: cnt, frames: frames, timestamp: Date.now() };
+	          if (srcData.callbacks) {
+	            srcData.callbacks.forEach(function (cb) {
+	              return cb(newData);
+	            });
+	            gifData[src] = newData;
+	          }
+	        }, function (err) {
+	          return onError(err);
+	        });
+	      });
+	    });
+	    tester.addEventListener('error', function (e) {
+	      return onError('Could be the following issue\n - Not Image\n - Not Found\n - Server Error\n - Cross-Origin Issue');
+	    });
+	    function onError(message) {
+	      var errData = createError(message, src);
+	      if (srcData.callbacks) {
+	        srcData.callbacks.forEach(function (cb) {
+	          return cb(errData);
+	        });
+	        gifData[src] = errData;
+	      }
+	    }
+	    tester.src = src;
+	  },
+
+	  __getUnit8Array: function __getUnit8Array(src, cb) {
+	    if (typeof cb !== 'function') {
+	      return;
+	    }
+
+	    var xhr = new XMLHttpRequest();
+	    xhr.open('GET', src);
+	    xhr.responseType = 'arraybuffer';
+	    xhr.addEventListener('load', function (e) {
+	      var uint8Array = new Uint8Array(e.target.response);
+	      var arr = uint8Array.subarray(0, 4);
+	      var header = '';
+	      for (var i = 0; i < arr.length; i++) {
+	        header += arr[i].toString(16);
+	      }
+	      if (header === '47494638') {
+	        cb(uint8Array);
+	      } else {
+	        cb();
+	      }
+	    });
+	    xhr.addEventListener('error', function (e) {
+	      log(e);
+	      cb();
+	    });
+	    xhr.send();
+	  },
+
+	  __validateAndGetQuerySelector: function __validateAndGetQuerySelector(selector) {
+	    try {
+	      var el = document.querySelector(selector);
+	      if (!el) {
+	        return { error: 'No element was found matching the selector' };
+	      }
+	      return el;
+	    } catch (e) {
+	      return { error: 'no valid selector' };
+	    }
+	  },
+
+	  __addPublicFunctions: function __addPublicFunctions() {
+	    this.el.gif = {
+	      play: this.play.bind(this),
+	      pause: this.pause.bind(this),
+	      togglePlayback: this.togglePlayback.bind(this),
+	      paused: this.paused.bind(this),
+	      nextFrame: this.nextFrame.bind(this)
+	    };
+	  },
+
+	  pause: function pause() {
+	    log('pause');
+	    this.__paused = true;
+	  },
+
+	  play: function play() {
+	    log('play');
+	    this.__paused = false;
+	  },
+
+	  togglePlayback: function togglePlayback() {
+	    if (this.paused()) {
+	      this.play();
+	    } else {
+	      this.pause();
+	    }
+	  },
+
+	  paused: function paused() {
+	    return this.__paused;
+	  },
+
+	  nextFrame: function nextFrame() {
+	    this.__draw();
+
+	    while (Date.now() - this.__startTime >= this.__nextFrameTime) {
+	      this.__nextFrameTime += this.__delayTimes[this.__frameIdx++];
+	      if ((this.__infinity || this.__loopCnt) && this.__frameCnt <= this.__frameIdx) {
+	        this.__frameIdx = 0;
+	      }
+	    }
+	  },
+
+	  __clearCanvas: function __clearCanvas() {
+	    this.__ctx.clearRect(0, 0, this.__width, this.__height);
+	    this.__texture.needsUpdate = true;
+	  },
+
+	  __draw: function __draw() {
+	     this.__clearCanvas(); this.__ctx.drawImage(this.__frames[this.__frameIdx], 0, 0, this.__width, this.__height); this.__texture.needsUpdate = true;
+	  },
+
+	  __ready: function __ready(_ref) {
+	    var src = _ref.src;
+	    var times = _ref.times;
+	    var cnt = _ref.cnt;
+	    var frames = _ref.frames;
+
+	    log('__ready');
+	    this.__textureSrc = src;
+	    this.__delayTimes = times;
+	    cnt ? this.__loopCnt = cnt : this.__infinity = true;
+	    this.__frames = frames;
+	    this.__frameCnt = times.length;
+	    this.__startTime = Date.now();
+	    this.__width = THREE.Math.nearestPowerOfTwo(frames[0].width);
+	    this.__height = THREE.Math.nearestPowerOfTwo(frames[0].height);
+	    this.__cnv.width = this.__width;
+	    this.__cnv.height = this.__height;
+	    this.__draw();
+	    if (this.__autoplay) {
+	      this.play();
+	    } else {
+	      this.pause();
+	    }
+	  },
+
+	  __reset: function __reset() {
+	    this.pause();
+	    this.__clearCanvas();
+	    this.__startTime = 0;
+	    this.__nextFrameTime = 0;
+	    this.__frameIdx = 0;
+	    this.__frameCnt = 0;
+	    this.__delayTimes = null;
+	    this.__infinity = false;
+	    this.__loopCnt = 0;
+	    this.__frames = null;
+	    this.__textureSrc = null;
+	  }
+	});
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	exports.parseGIF = function (gif, successCB, errorCB) {
+
+	  var pos = 0;
+	  var delayTimes = [];
+	  var loadCnt = 0;
+	  var graphicControl = null;
+	  var imageData = null;
+	  var frames = [];
+	  var loopCnt = 0;
+	  if (gif[0] === 0x47 && gif[1] === 0x49 && gif[2] === 0x46 &&
+	  gif[3] === 0x38 && gif[4] === 0x39 && gif[5] === 0x61) {
+	    pos += 13 + +!!(gif[10] & 0x80) * Math.pow(2, (gif[10] & 0x07) + 1) * 3;
+	    var gifHeader = gif.subarray(0, pos);
+	    while (gif[pos] && gif[pos] !== 0x3b) {
+	      var offset = pos,
+	          blockId = gif[pos];
+	      if (blockId === 0x21) {
+	        var label = gif[++pos];
+	        if ([0x01, 0xfe, 0xf9, 0xff].indexOf(label) !== -1) {
+	          label === 0xf9 && delayTimes.push((gif[pos + 3] + (gif[pos + 4] << 8)) * 10);
+	          label === 0xff && (loopCnt = gif[pos + 15] + (gif[pos + 16] << 8));
+	          while (gif[++pos]) {
+	            pos += gif[pos];
+	          }label === 0xf9 && (graphicControl = gif.subarray(offset, pos + 1));
+	        } else {
+	          errorCB && errorCB('parseGIF: unknown label');break;
+	        }
+	      } else if (blockId === 0x2c) {
+	        pos += 9;
+	        pos += 1 + +!!(gif[pos] & 0x80) * (Math.pow(2, (gif[pos] & 0x07) + 1) * 3);
+	        while (gif[++pos]) {
+	          pos += gif[pos];
+	        }var imageData = gif.subarray(offset, pos + 1);
+	        frames.push(URL.createObjectURL(new Blob([gifHeader, graphicControl, imageData])));
+	      } else {
+	        errorCB && errorCB('parseGIF: unknown blockId');break;
+	      }
+	      pos++;
+	    }
+	  } else {
+	    errorCB && errorCB('parseGIF: no GIF89a');
+	  }
+	  if (frames.length) {
+
+	    var cnv = document.createElement('canvas');
+	    var loadImg = function loadImg() {
+	      frames.forEach(function (src, i) {
+	        var img = new Image();
+	        img.onload = function (e, i) {
+	          if (i === 0) {
+	            cnv.width = img.width;
+	            cnv.height = img.height;
+	          }
+	          loadCnt++;
+	          frames[i] = this;
+	          if (loadCnt === frames.length) {
+	            loadCnt = 0;
+	            imageFix(1);
+	          }
+	        }.bind(img, null, i);
+	        img.src = src;
+	      });
+	    };
+	    var imageFix = function imageFix(i) {
+	      var img = new Image();
+	      img.onload = function (e, i) {
+	        loadCnt++;
+	        frames[i] = this;
+	        if (loadCnt === frames.length) {
+	          cnv = null;
+	          successCB && successCB(delayTimes, loopCnt, frames);
+	        } else {
+	          imageFix(++i);
+	        }
+	      }.bind(img);
+	      img.src = cnv.toDataURL('image/gif');
+	    };
+	    loadImg();
+	  }
+	};
+
+/***/ }
+/******/ ]);
